edb33d695c9ea10045ee10d8ca4ab610c5d38766
Долгов Роман Андреевич <rad2312@mail.ru>
2024-10-13 21:38:08 +0300

    Update PkmnApplication.java

--- src/main/java/ru/mirea/dolgov/pkmn/PkmnApplication.java
+++ src/main/java/ru/mirea/dolgov/pkmn/PkmnApplication.java
@@ -1,4 +1,25 @@
 package ru.mirea.dolgov.pkmn;
 
+
+import java.io.IOException;
+
 public class PkmnApplication {
+
+    public static void main(String[] args) {
+
+        String cardFilePath = "C:/Users/user/IdeaProjects/Pkmn/src/main/resources/my_card.txt";
+
+        try {
+
+            Card importedCard = CardImport.importCard(cardFilePath);
+
+
+            System.out.println("Импортированная карта покемона:");
+            System.out.println(importedCard);
+
+        } catch (IOException e) {
+            System.err.println("Ошибка при загрузке данных карты: " + e.getMessage());
+        }
+    }
 }
+
